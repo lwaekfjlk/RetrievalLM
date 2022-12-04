edf5c9d0159a1b63451233a4f2ab4411e54de6ad
Guanzhong Liu <rogerliu00425@gmail.com>
2022-12-04 10:58:16 -0500

    add scripts

---
+++ scripts/eval_base.sh
@@ -0,0 +1,9 @@
+MODEL=gpt2-code-finetune
+
+python -u run_clm.py \
+  --model_name_or_path /bos/tmp15/guanzhol/rlm/checkpoints/${MODEL} \
+  --dataset_name Fraser/python-lines \
+  --output_dir ./outputs/${MODEL}/eval_base \
+  --do_eval \
+  --max_eval_samples 3000 \
+  --eval_subset validation
